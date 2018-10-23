e93d6eb9427fd1c0d5adc9e13e68a2b126f7c354
Renée Kooi <renee@kooi.me>
2018-10-23 17:08:07 +0200

    aliases: add `default` branch support to gcm

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,8 @@ mkcd() {
   cd "$1"
 }
 
+# Move into a directory and list the contents.
+# `cdl /path/to/directory`
 cdl() {
   cd "$1"
   ls
@@ -12,3 +14,7 @@ cdl() {
 
 alias mkcd=mkcd
 alias cdl=cdl
+
+# this is "git checkout master" by default in oh-my-zsh
+# but i like to use the `default` branch name
+alias gcm="git checkout master 2>/dev/null || git checkout default"
