3fdbc2e1472afd81a7d91f9051d786d5a5128b98
Renée Kooi <renee@kooi.me>
2019-11-12 16:55:14 +0100

    alias: use exa if available

--- aliases.sh
+++ aliases.sh
@@ -1,20 +1,24 @@
 # Create a directory and move into it.
 # `mkcd /path/to/directory`
-mkcd() {
+_mkcd() {
   mkdir -p "$1"
   cd "$1"
 }
 
 # Move into a directory and list the contents.
 # `cdl /path/to/directory`
-cdl() {
+_cdl() {
   cd "$1"
   ls
 }
 
-alias mkcd=mkcd
-alias cdl=cdl
+alias mkcd=_mkcd
+alias cdl=_cdl
 
 # this is "git checkout master" by default in oh-my-zsh
 # but i like to use the `default` branch name
 alias gcm="git checkout master 2>/dev/null || git checkout default"
+
+if which exa > /dev/null; then
+  alias l="exa -l"
+fi
