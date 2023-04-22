cf8f3e6dc0f46ecd2ee9e47dd4eef964fea9a13e
Renée Kooi <renee@kooi.me>
2023-04-22 16:09:24 +0200

    nvim: add autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,16 +12,21 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+	-- Theme
+	'dracula/vim',
+
 	'tpope/vim-repeat',
 	'tpope/vim-commentary',
 	'tpope/vim-surround',
-	-- 'folke/tokyonight.nvim',
-	-- 'rebelot/kanagawa.nvim',
-	'dracula/vim',
 
 	'nvim-treesitter/nvim-treesitter',
 	'neovim/nvim-lspconfig',
 	'simrat39/rust-tools.nvim',
+
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'hrsh7th/nvim-cmp',
 })
 
 vim.cmd 'colorscheme dracula'
@@ -111,3 +116,39 @@ require('nvim-treesitter.configs').setup {
 	highlight = { enable = true },
 	indent = { enable = true }
 }
+
+local cmp = require('cmp')
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = 'buffer' },
+	})
+})
